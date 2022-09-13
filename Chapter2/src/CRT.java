1d89d127e1875351ec687d574344e866c4fff849
1005244507 <1005244507@L815WD220004.edu.cbe.ab.ca>
2022-09-13 09:35:23 -0600

    9:35

--- Chapter2/src/CRT.java
+++ Chapter2/src/CRT.java
@@ -10,13 +10,26 @@ public class CRT
 
 	public static void main(String[] args) 
 	{
-		/*
+		/* 3.
+			/*
 			Program: Average.java  
 			Calculates test averages        
 			Author: Kearmouy Heng
 			School: CHHS
 			Course: Computer Science 20
  			Date: 2022-09-13
+ 			
+ 			4. 
+ 			// The AverageScore class displays the average of a set of scores
+ 			
+ 			5.
+ 			7.
+ 			A source code is the code that the programmer typed. It must be able to be 
+ 			made, executed, and be able to run. Byte code is what the machine understands. 
+ 			
+ 			12.
+ 			print() will display the output onto the screen. println() displays the output 
+ 			onto the screen onto the next line.
 		 */
 	}
 
